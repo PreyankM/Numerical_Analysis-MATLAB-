fdd5d5286aa94dc5aa53182f10cbb6bc40a175eb
PreyankM <78067405+PreyankM@users.noreply.github.com>
2021-11-27 19:34:17 +0530

    Update newtons.m

--- newtons.m
+++ newtons.m
@@ -1,5 +1,4 @@
 %NEWTON'S METHOD
-%Author : Preyank Mota - 2019B4A70331G
 %Newton's method is a numerical method to find approximate root of a
 %function.
 
@@ -8,10 +7,6 @@ format long g; % Setting up format for output screen.
 %Initializing the function to find the root. Calling the function to
 %calculate the root.
 f = @(x)cos(x)-x;
- 
-%Adding 'x' on both sides to convert fixed point finding 
-%problem to root finding problem.
-f2 = @(x)cos(x);
 
 disp("Change function inside program('.m' file).");
 init = input("Enter initial approximation : ");%0.7853981635 for given example
@@ -20,7 +15,6 @@ nmax = input("Enter maximum number of iterations allowed : ");%20 for given exam
 
 %Function Parameters consist of 1.function(in the form of @(x)),2. Initial approximation(p_0), 
 %3. Tolerence limit, 4. Maximum number of iterations(N_0)
-s1 = fixedPoint(f2,init,TOL,nmax);%Using N_0=7,as example requires only 7 iterations.
 s = newton(f,init,TOL,nmax);
 
 %Function for calculating root by Newton's method.
@@ -63,40 +57,4 @@ disp("NEWTON'S METHOD");
     %Printing final table onto the output screen.
     newtonMethod = table(n_arr,p);
     disp(newtonMethod);
-end
-
-function p = fixedPoint(g,p0,error,max_iters)
-disp("FIXED POINT ITERATION METHOD");
-
-%Initializing function parameters
-MAX_VALUE = 999999999999999; %Max value parameter, so that iteration values do not go very high.
-
-ctr = 0;
-p = [p0];
-new_p = 0;
-n_arr = [ctr];
-
-while(ctr < max_iters)
-    
-    new_p = g(p0); %Iterative scheme
-    p = [p; new_p];
-    
-    %Stopping criteria(Extra criteria used so that values of p_n does not blow up)
-    if((abs(new_p-p0) < error) || (abs(new_p) > MAX_VALUE))
-        break;
-    end
-    
-    %Updating parameters.
-    p0 = new_p;
-    ctr = ctr + 1;
-    n_arr = [n_arr; ctr];
-end
-
-if(ctr == max_iters + 1)
-    fprintf("Method failed after N_0 iterations, N_0 = %d\n", max_iters);
-end
-
-roots = table(n_arr,p);
-disp(roots);
-    
 end
